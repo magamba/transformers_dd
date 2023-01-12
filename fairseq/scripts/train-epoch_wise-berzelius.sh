90585f7b15d2d8c75ac5b53602192abc8e906c4a
Matteo Gamba <x_matga@berzelius001.eth.cluster>
2023-01-12 23:39:07 +0100

    Updated training scripts.

--- fairseq/scripts/train-epoch_wise-berzelius.sh
+++ fairseq/scripts/train-epoch_wise-berzelius.sh
@@ -5,8 +5,8 @@ workers=0
 base_model="transformer_vaswani"
 seed=1
 
-# array size: 0-970:2%8
+# array size: 0-127:2%16
 
 sbatch --export NAME=$name,BASE_MODEL=$base_model,WORKERS=$workers,SEED=$seed \
-       --array=0-970:2%8 \
-       ../exps/train-epoch_wise-berzelius.sbatch
+       --array=0-127:2%16 \
+       exps/train-epoch_wise-berzelius.sbatch
