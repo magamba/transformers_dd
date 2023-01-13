d36489622a1383396de7e0f79828f26fe89daed9
Matteo Gamba <matteoga@alvis1.int.private>
2023-01-13 01:13:53 +0100

    Alvis training scripts.

---
+++ fairseq/scripts/train-epoch_wise-alvis.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+name="checkpoints_transformers"
+workers=0
+base_model="transformer_vaswani"
+seed=1
+
+# array size: 0-127:2%16
+
+sbatch --export NAME=$name,BASE_MODEL=$base_model,WORKERS=$workers,SEED=$seed \
+       --array=0 \
+       exps/train-epoch_wise-alvis.sbatch
