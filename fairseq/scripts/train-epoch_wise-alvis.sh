870121310ee60e0741ca495fb3665cffc7b00c76
Matteo Gamba <matteoga@alvis1.int.private>
2023-01-18 14:10:42 +0100

    One job array per dataset.

--- fairseq/scripts/train-epoch_wise-alvis.sh
+++ fairseq/scripts/train-epoch_wise-alvis.sh
@@ -3,11 +3,15 @@
 name="checkpoints_transformers"
 workers=0
 base_model="transformer_vaswani"
-seed=1
+seed=3
 
 # array size:
 #     islwt'14: 0-126:2%16
 #     wmt'14: 1-127:2%16
+
+sbatch --export NAME=$name,BASE_MODEL=$base_model,WORKERS=$workers,SEED=$seed \
+       --array=0-126:2%16 \
+       exps/train-epoch_wise-alvis.sbatch
 
 sbatch --export NAME=$name,BASE_MODEL=$base_model,WORKERS=$workers,SEED=$seed \
        --array=1-127:2%16 \
