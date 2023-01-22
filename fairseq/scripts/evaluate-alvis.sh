98683722f0386a266aededc22a34be6b218fc245
Matteo Gamba <matteoga@alvis1.int.private>
2023-01-22 20:58:25 +0100

    Support for metrics and checkpoints.

--- fairseq/scripts/evaluate-alvis.sh
+++ fairseq/scripts/evaluate-alvis.sh
@@ -5,34 +5,43 @@ workers=0
 base_model="transformer_vaswani"
 seeds=(
     1
-    2
-    3
+#    2
+#    3
+)
+
+splits=(
+    "train"
+#    "test"
+#    "valid"
+)
+
+declare -A checkpoints=(
+   ["iwslt14_de_en"]=290
+   ["wmt14_en_fr"]=115
+)
+
+metrics=(
+#    "perplexity"
+    "jacobian"
 )
 
 # array size:
 #     islwt'14: 0-126:2%16
 #     wmt'14: 1-127:2%16
 
-splits=(
-    "train"
-    "valid"
-    "test"
-)
-
-checkpoints=(
-    "last"
-)
-
 for seed in ${seeds[@]}; do
     for split in ${splits[@]}; do
-        for checkpoint in ${checkpoints[@]}; do
-            sbatch --export NAME=$name,BASE_MODEL=$base_model,WORKERS=$workers,SEED=$seed,SPLIT=$split,CHECKPOINT=$checkpoint \
-                   --array=0-126:2%16 \
-                   exps/evaluate-alvis.sbatch
-
-            sbatch --export NAME=$name,BASE_MODEL=$base_model,WORKERS=$workers,SEED=$seed,SPLIT=$split,CHECKPOINT=$checkpoint \
-                   --array=1-127:2%16 \
-                   exps/evaluate-alvis.sbatch
+        for metric in ${metrics[@]}; do
+            for checkpoint in ${checkpoints["iwslt14_de_en"]}; do
+                sbatch --export NAME=$name,BASE_MODEL=$base_model,WORKERS=$workers,SEED=$seed,SPLIT=$split,CHECKPOINT=$checkpoint,METRIC=$metric \
+                       --array=0 \
+                       exps/evaluate-alvis.sbatch
+            done
+            for checkpoint in ${checkpoints["wmt14_en_fr"]}; do
+                sbatch --export NAME=$name,BASE_MODEL=$base_model,WORKERS=$workers,SEED=$seed,SPLIT=$split,CHECKPOINT=$checkpoint,METRIC=$metric \
+                       --array=1 \
+                       exps/evaluate-alvis.sbatch
+            done
         done
     done
 done
