9766311f2a469da6277f33ab7ef461b1919e2f7a
Matteo Gamba <matteoga@alvis1.int.private>
2023-01-23 02:43:07 +0100

    Removing debug statements.

--- fairseq/loss_jacobian_norm.py
+++ fairseq/loss_jacobian_norm.py
@@ -58,7 +58,6 @@ class SequenceScorer(object):
     def generate(self, models, sample, **kwargs):
         """Score a batch of translations."""
         net_input = sample["net_input"]
-        logger.info("Input: {}".format(net_input["src_tokens"].shape))
         def batch_for_softmax(dec_out, target):
             # assumes decoder_out[0] is the only thing needed (may not be correct for future models!)
             first, rest = dec_out[0], dec_out[1:]
@@ -186,10 +185,6 @@ class SequenceScorer(object):
         scores /= bsz
         scores.backward(gradient=torch.ones_like(scores))
         jacobian = torch.matmul(gradients_dict["upstream"].grad, gradients_dict["local"].T)
-        logger.info(f"avg_probs: {avg_probs.shape}")
-        logger.info("jacobian_embedding: {}".format(gradients_dict["upstream"].grad.shape))
-        logger.info(f"scores: {scores.shape}")
-        logger.info(f"jacobian: {jacobian.shape}")
         hypos.append(
             torch.norm(
                 jacobian,
@@ -197,7 +192,6 @@ class SequenceScorer(object):
                 dim=-1,
             ).view(-1).cpu().tolist()
         )
-        logger.info("jacobian norm: {}".format(jacobian.view(-1, gradients_dict["local"].shape[0]).shape))
         gradients_dict["upstream"].grad = None
         gradients_dict["upstream"] = None
         gradients_dict["local"] = None
